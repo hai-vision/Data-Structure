059b7ea2ff1a62704b35cea862d12409fef2aa26
hai-vision <62526131+hai-vision@users.noreply.github.com>
2021-07-24 22:32:49 +0800

    增加测试用例

--- SquList/test.c
+++ SquList/test.c
@@ -6,16 +6,17 @@ void test() {
 	SqList L;
 	SquListInit(&L);
 	SquListPush(&L, 1);
-	SquListPush(&L, 1);
-	SquListPush(&L, 1);
-	SquListPush(&L, 1);
-	SquListPush(&L, 1);
-	SquListPush(&L, 1);
-	printf("%d\t%d\n", L.capacity, L.length);
+	SquListPush(&L, 2);
+	SquListPush(&L, 3);
+	SquListPush(&L, 4);
+	/*SquListPushFront(&L, 0);
+	SquListPushFront(&L, 7);*/
 
-	
+	SquListPopFront(&L);
 	SquListPrint(&L);
 
+	/*SquListPop(&L);
+	SquListPrint(&L);*/
 
 }
 int main(){
