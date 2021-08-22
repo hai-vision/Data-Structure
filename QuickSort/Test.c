e62a4b54db6c545819aaa3e7487ad15aad19f010
hai-vision <62526131+hai-vision@users.noreply.github.com>
2021-08-22 11:16:32 +0800

    Update Test.c

--- QuickSort/Test.c
+++ QuickSort/Test.c
@@ -1,11 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "QuickSort.h"
 
+void Print(int* a, int n) 
+{
+	for (int i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+}
+void TestSort() 
+{
+	int arr[] = { 49,38,65,97,49,13,27,76 };
+	QuickSort(arr, 0, 7);
+	Print(arr, 7);
+}
 
 
 int main(){
 	
-	
-
+	TestSort();
 	return 0;
 }
